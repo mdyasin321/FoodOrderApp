205a4753ade5c99cf9fab658e1d2994f46154f5c
Yasin <mdyasinansari901@gmail.com>
2023-01-28 12:11:19 +0530

    added the Header Component

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import mealsImage from '../../Assets/meals.jpg';
+import classes from './Header.module.css';
+
+const Header =()=>{
+    return (
+            <React.Fragment>
+                <header className={classes.header}>
+                    <h1>React Meals</h1>
+                    <button><h1>Cart</h1></button>
+                </header>
+                {/* here the classes.main-header syntax is wrong because the class name should only contains number or alphabets and no dash or 
+                underslash. But still you want to use dash then you have to write like classes['main-header'] as written below */}
+                {/* <div className={classes.main-header}> */}
+                <div className={classes['main-image']}>
+                    <img src={mealsImage} alt='a delecious food' />
+                </div>
+            </React.Fragment>
+    )
+}
+export default Header;
