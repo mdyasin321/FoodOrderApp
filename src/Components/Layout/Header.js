f0d2317987d2f886f14d38e826a178035ccbfe9f
Yasin <mdyasinansari901@gmail.com>
2023-01-28 12:41:28 +0530

    added the cart button

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import mealsImage from '../../Assets/meals.jpg';
 import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header =()=>{
     return (
             <React.Fragment>
                 <header className={classes.header}>
                     <h1>React Meals</h1>
-                    <button><h1>Cart</h1></button>
+                    <HeaderCartButton></HeaderCartButton>
                 </header>
                 {/* here the classes.main-header syntax is wrong because the class name should only contains number or alphabets and no dash or 
                 underslash. But still you want to use dash then you have to write like classes['main-header'] as written below */}
